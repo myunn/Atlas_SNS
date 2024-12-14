1ab3c7d892e03e7c437430f394a31288d229f20a
Myun3\myun <momon0.0peach@gmail.com>
2024-12-14 18:41:14 +0900

    『新規ユーザーはこちら』に白色の下線を付ける

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,15 +6,15 @@
 
 <p>AtlasSNSへようこそ</p>
 
-{{ Form::label('e-mail') }}
+{{ Form::label('メールアドレス') }}
 {{ Form::text('mail',null,['class' => 'input']) }}
 <br>
-{{ Form::label('password') }}
+{{ Form::label('パスワード') }}
 {{ Form::password('password',['class' => 'input']) }}
 
 {{ Form::submit('ログイン') }}
 
-<p><a href="/register">新規ユーザーの方はこちら</a></p>
+<p class='newuser'><a href="/register">新規ユーザーの方はこちら</a></p>
 
 {!! Form::close() !!}
 
