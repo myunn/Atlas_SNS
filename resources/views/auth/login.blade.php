c76a38f7483cb613fb1d7275796e208589bd84cd
Myun3\myun <momon0.0peach@gmail.com>
2024-12-20 17:56:56 +0900

    login＋新規登録画面の囲い作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,21 @@
 <!-- viewファイルにあるフォームファサードで送るデータの記述先の記載。 -->
 {!! Form::open(['url' => '/login']) !!}
 
-<p>AtlasSNSへようこそ</p>
+<div class="wrapper">
+
+<p class='welcome'>AtlasSNSへようこそ</p>
 
 {{ Form::label('メールアドレス') }}
 {{ Form::text('mail',null,['class' => 'input']) }}
 <br>
 {{ Form::label('パスワード') }}
 {{ Form::password('password',['class' => 'input']) }}
-
+<br>
 {{ Form::submit('ログイン') }}
-
+<br>
 <p class='newuser'><a href="/register">新規ユーザーの方はこちら</a></p>
-
+<br>
 {!! Form::close() !!}
 
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,23 +4,27 @@
 <!-- viewファイルにあるフォームファサードで送るデータの記述先の記載。 -->
 {!! Form::open(['url' => '/register']) !!}
 
+<div class="wrapper">
+
 <h2>新規ユーザー登録</h2>
 
 {{ Form::label('ユーザー名') }}
 {{ Form::text('username',null,['class' => 'input']) }}
-
+<br>
 {{ Form::label('メールアドレス') }}
 {{ Form::text('mail',null,['class' => 'input']) }}
-
+<br>
 {{ Form::label('パスワード') }}
 {{ Form::text('password',null,['class' => 'input']) }}
-
+<br>
 {{ Form::label('パスワード確認') }}
 {{ Form::text('password_confirmation',null,['class' => 'input']) }}
-
+<br>
 {{ Form::submit('新規登録') }}
 
 <p><a href="/login">ログイン画面へ戻る</a></p>
+
+<div class="wrapper">
 
 {!! Form::close() !!}
 
