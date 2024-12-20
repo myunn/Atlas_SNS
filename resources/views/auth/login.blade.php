0d42ea9f7a63375f8fa747d7014d3bbdb2fc3b8c
Myun3\myun <momon0.0peach@gmail.com>
2024-12-20 21:43:22 +0900

    login(logout)レイアウト修正２

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,11 +4,13 @@
 <!-- viewファイルにあるフォームファサードで送るデータの記述先の記載。 -->
 {!! Form::open(['url' => '/login']) !!}
 
+<!-- 構成を『div』で3つに分ける -->
 <div class="wrapper">
 
 <p class="welcome">AtlasSNSへようこそ</p>
 
 <div>
+  <!-- 入力欄ごとにさらに構成を『div』で分ける -->
   <div class="form-input">
     {{ Form::label('メールアドレス') }}
     {{ Form::text('mail',null,['class' => 'input']) }}
@@ -18,7 +20,7 @@
     {{ Form::password('password',['class' => 'input']) }}
   </div>
 </div>
-<div>
+<div class="loginbtn">
   {{ Form::submit('ログイン') }}
   <p class='newuser'><a href="/register">新規ユーザーの方はこちら</a></p>
 </div>
