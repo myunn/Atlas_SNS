c880c6c3f63a4ec7588d61cebe0234575625fa79
Myun3\myun <momon0.0peach@gmail.com>
2025-01-13 15:02:54 +0900

    ボタン調整

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-// 記載：ハンバーガーメニュー（アニメーション的な動きを追加）
+// 記載)ハンバーガーメニュー（アニメーション的な動きを追加）
 $(function () {
   $('.nav-btn').click(function () {
     $(this).toggleClass('active');
@@ -13,3 +13,29 @@ $(function () {
   // $('.nav-menu').removeClass('active');         //(.g-navi)にある（.active）も削除する。
   // });
 });
+
+//記載) モーダル機能の動き追加
+//要素を取得
+const modal = document.querySelector('.js-modal'),
+  open = document.querySelector('.js-modal-open'),
+  close = document.querySelector('.js-modal-close');
+
+//「開くボタン」をクリックしてモーダルを開く
+function modalOpen() {
+  modal.classList.add('is-active');
+}
+open.addEventListener('click', modalOpen);
+
+//「閉じるボタン」をクリックしてモーダルを閉じる
+function modalClose() {
+  modal.classList.remove('is-active');
+}
+close.addEventListener('click', modalClose);
+
+//「モーダルの外側」をクリックしてモーダルを閉じる
+function modalOut(e) {
+  if (e.target == modal) {
+    modal.classList.remove('is-active');
+  }
+}
+addEventListener('click', modalOut);
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -8,4 +8,12 @@
 <h1><a href="/user"><img src="http://127.0.0.1:8000/images/search.png"></a></h1>
 </form>
 
+<!-- フォロー・フォロー解除ボタン -->
+<div class="followbutton">
+ <button id="follow-button"><a href="/search">フォローする</a></button>
+</div>
+<div class="unfollowbtn">
+ <button id="unfollow-button"><a href="/search">フォロー解除</a></button>
+</div>
+
 @endsection
