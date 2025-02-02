87fa55ebecfe8683ce0e13e81e0e0c757326f002
Myun3\myun <momon0.0peach@gmail.com>
2025-02-02 18:27:52 +0900

    profileページのCSS修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,24 +7,30 @@
 <div class="wrapper">
 
 <h2>新規ユーザー登録</h2>
+<form action="profile.blade.php" method="post">
+  <li class="profile">
+    <li class=yoko>
+      <label>ユーザー名</label>
+    </li>
+  </li>
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
-<br>
-{{ Form::label('メールアドレス') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
-<br>
-<form>
-  <label for="password">パスワード</label>
-  <input type="password" id="password" name="password">
+  {{ Form::label('ユーザー名') }}
+  {{ Form::text('username',null,['class' => 'input']) }}
+
+  <br>
+  {{ Form::label('メールアドレス') }}
+  {{ Form::text('mail',null,['class' => 'input']) }}
+  <br>
+
+  {{ Form::label('パスワード') }}
+  {{ Form::text('password',null,['class' => 'input']) }}
+  <br>
+
+  {{ Form::label('パスワード確認') }}
+  {{ Form::text('password_confirmation',null,['class' => 'input']) }}
+  <br>
+  {{ Form::submit('新規登録') }}
 </form>
-<!-- {{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }} -->
-<br>
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation',null,['class' => 'input']) }}
-<br>
-{{ Form::submit('新規登録') }}
 
 <p><a href="/login">ログイン画面へ戻る</a></p>
 
--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -4,44 +4,44 @@
 
 <!-- 記述： プロフィール内容-->
 
-<div class="user_image_name">
-<h5><a href="/top"><img src="http://127.0.0.1:8000/images/icon1.png"></a></h5>
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
-</div>
+<div class="profile">
+  <div>
+    <h5><a href="/top"><img src="http://127.0.0.1:8000/images/icon1.png"></a></h5>
+    {{ Form::label('ユーザー名') }}
+    {{ Form::text('username',null,['class' => 'input']) }}
+  </div>
 
-<div>
-{{ Form::label('メールアドレス') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
-</div>
+  <div>
+  {{ Form::label('メールアドレス') }}
+  {{ Form::text('mail',null,['class' => 'input']) }}
+  </div>
 
-<div>
-{{ Form::label('パスワード') }}
-{{ Form::text('password',null,['class' => 'input']) }}
-</div>
+  <div>
+  {{ Form::label('パスワード') }}
+  {{ Form::text('password',null,['class' => 'input']) }}
+  </div>
 
-<div>
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation',null,['class' => 'input']) }}
-</div>
+  <div>
+  {{ Form::label('パスワード確認') }}
+  {{ Form::text('password_confirmation',null,['class' => 'input']) }}
+  </div>
 
-<div>
-{{ Form::label('自己紹介') }}
-{{ Form::text('text',null,['class' => 'input']) }}
-</div>
+  <div>
+  {{ Form::label('自己紹介') }}
+  {{ Form::text('text',null,['class' => 'input']) }}
+  </div>
 
-<div>
-{{ Form::label('アイコン画像') }}
-{{ Form::text('picture',null,['class' => 'input']) }}
-<!-- 上だけでいける？ -->
+  <div>
+  {{ Form::label('アイコン画像') }}
+  {{ Form::text('picture',null,['class' => 'input']) }}
+  <!-- 上だけでいける？ -->
   <input type="text" name="post" placeholder="画像を挿入">
+  </div>
+
+  <!-- ボタンに機能を追加する -->
+  <div class="updatebtn">
+  <button id="update-button"><a href="/top">更新</a></button>
+  </div>
 </div>
-
- <!-- ボタンに機能を追加する -->
-<div class="updatebtn">
-<button id="update-button"><a href="/top">更新</a></button>
-
-</div>
-
 
 @endsection
