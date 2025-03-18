41390b92de02ff6962f5c91b0ac823b869b0d16b
Myun3\myun <momon0.0peach@gmail.com>
2025-03-18 18:47:10 +0900

    初期値の反映と入力欄へプレースホルダーの反映

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -5,35 +5,35 @@
 <!-- 記述： プロフィール内容-->
 
 <div class="profile">
-  <div>
+  <div class="profile_A">
     <h5><a href="/top"><img src="images/{{Auth::user()->images}}"></a></h5>
     {{ Form::label('ユーザー名') }}
-    {{ Form::text('username',null,['class' => 'input']) }}
+    {{ Form::text('username',Auth::user()->username,null,['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('メールアドレス') }}
-  {{ Form::text('mail',null,['class' => 'input']) }}
+  {{ Form::text('mail',Auth::user()->mail,null,['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('パスワード') }}
-  {{ Form::text('password',null,['class' => 'input']) }}
+  {{ Form::text('password',Auth::user()->password,null,['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('パスワード確認') }}
-  {{ Form::text('password_confirmation',null,['class' => 'input']) }}
+  {{ Form::text('password_confirmation',Auth::user()->password,null,['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('自己紹介') }}
-  {{ Form::text('text',null,['class' => 'input']) }}
+  {{ Form::text('text',Auth::user()->text,null,['class' => 'input']) }}
   </div>
 
   <div>
   {{ Form::label('アイコン画像') }}
-  {{ Form::text('picture',null,['class' => 'input','placeholder' => '画像を挿入']) }}
+  {{ Form::text('picture',null,['class' => 'input','placeholder' => 'ファイルを選択']) }}
   </div>
 
   <!-- ボタンに機能を追加する -->
