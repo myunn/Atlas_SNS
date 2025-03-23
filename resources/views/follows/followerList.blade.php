71573e1dec9547a546f27f06bc19dc5d55087041
Myun3\myun <momon0.0peach@gmail.com>
2025-03-23 15:07:31 +0900

    フォロワーリストから相手ユーザーのプロフィールへの遷移

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -6,7 +6,7 @@
 <p class="follower-list-title">フォロワーリスト</p>
   @foreach ($following_users as $following_user)
   <div class=all_follow-users_icon>
-    <a href="/users_profile"><img src="images/{{$following_user->images}}" class="follow-List"></a>
+    <a href="/users_profile/{{$following_user->id}}"><img src="images/{{$following_user->images}}" class="follow-List"></a>
   </div>
   @endforeach
 </div>
